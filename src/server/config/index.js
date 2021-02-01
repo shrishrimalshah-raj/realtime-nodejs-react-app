1c6943d5a8283f586b87ba3a162f98f168cd0d8b
shrishrimalshah-raj <shrishrimalraj@gmail.com>
2021-02-02 01:53:08 +0530

    fixed error

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -5,8 +5,8 @@ const envVars = process.env;
 const config = {
   url: envVars.NODE_ENV === "dev" ? "mongodb://localhost:27018" : "mongodb://localhost:27017",
   dbName: "oi-data-analysis",
-  collectionNameBankNiftyOptionChainOI: "banknifty-option-chain-oi",
-  collectionNameBankNiftyFuturesOI: "banknifty-futures-oi",
+  collectionNameBankNiftyOptionChainOI: "bankniftyOptionChainOI",
+  collectionNameBankNiftyFuturesOI: "bankniftyFuturesOI",
 };
 
 export { config };
