c6583ca340eef8956cbbed367748e5ef0ab5eca8
shrishrimalshah-raj <shrishrimalraj@gmail.com>
2021-02-03 01:17:16 +0530

    fixed styling

--- src/client/components/BankNiftyChart.js
+++ src/client/components/BankNiftyChart.js
@@ -31,70 +31,77 @@ const BankNiftyChart = (props) => {
   const { bankNiftyoptionChainData, bankNiftyFutureData } = props;
   return (
     <div>
-            <h1>Bank Nifty Future Chart</h1>
+      <h1 style={{ marginLeft: "200px" }}>Bank Nifty Future Chart</h1>
       {bankNiftyFutureData.length > 0 ? (
         <>
-          <Chart
-            width={"60%"}
-            height={"500"}
-            chartType="Line"
-            loader={<div>Loading Chart</div>}
-            data={returnBankNiftyFutureData(bankNiftyFutureData)}
-            options={{
-              chart: {
-                title: "Bank Nifty and Open Interest Chart For Future Data",
-              },
-              width: 1000,
-              height: 500,
-              series: {
-                // Gives each series an axis name that matches the Y-axis below.
-                0: { axis: "openInterest" },
-                1: { axis: "underlyingValue" },
-              },
-              axes: {
-                // Adds labels to each axis; they don't have to match the axis names.
-                y: {
-                  openInterest: { label: "openInterest" },
-                  underlyingValue: { label: "underlyingValue" },
+          <div style={{ marginLeft: "200px" }}>
+            <Chart
+              width={"60%"}
+              height={"500"}
+              chartType="Line"
+              loader={<div>Loading Chart</div>}
+              data={returnBankNiftyFutureData(bankNiftyFutureData)}
+              options={{
+                chart: {
+                  title: "Bank Nifty and Open Interest Chart For Future Data",
                 },
-              },
-            }}
-            rootProps={{ "data-testid": "4" }}
-          />
+                width: 1000,
+                height: 500,
+                series: {
+                  // Gives each series an axis name that matches the Y-axis below.
+                  0: { axis: "openInterest" },
+                  1: { axis: "underlyingValue" },
+                },
+                axes: {
+                  // Adds labels to each axis; they don't have to match the axis names.
+                  y: {
+                    openInterest: { label: "openInterest" },
+                    underlyingValue: { label: "underlyingValue" },
+                  },
+                },
+              }}
+              rootProps={{ "data-testid": "4" }}
+            />
+          </div>
         </>
       ) : null}
       <br />
       <br />
-      <h1>Bank Nifty Option Chain Chart</h1>
+      <h1 style={{ marginLeft: "200px" }}>Bank Nifty Option Chain Chart</h1>
       {bankNiftyoptionChainData.length > 0 ? (
         <>
-          <Chart
-            width={"60%"}
-            height={"500"}
-            chartType="Line"
-            loader={<div>Loading Chart</div>}
-            data={returnBankNiftyOptionChainData(bankNiftyoptionChainData)}
-            options={{
-              chart: {
-                title: "Bank Nifty and Open Interest Chart For Option Chain Data",
-              },
-              width: 1050,
-              height: 500,
-              series: {
-                // Gives each series an axis name that matches the Y-axis below.
-                0: { axis: "diffChangeInOpenInterest" },
-                1: { axis: "underlyingValue" },
-              },
-              axes: {
-                // Adds labels to each axis; they don't have to match the axis names.
-                y: {
-                  diffChangeInOpenInterest: { label: "diffChangeInOpenInterest" },
-                  underlyingValue: { label: "underlyingValue" },
+          <div style={{ marginLeft: "200px" }}>
+            <Chart
+              width={"60%"}
+              height={"500"}
+              chartType="Line"
+              loader={<div>Loading Chart</div>}
+              data={returnBankNiftyOptionChainData(bankNiftyoptionChainData)}
+              options={{
+                chart: {
+                  title:
+                    "Bank Nifty and Open Interest Chart For Option Chain Data",
                 },
-              },
-            }}
-            rootProps={{ "data-testid": "4" }}
-          />
+                width: 1050,
+                height: 500,
+                series: {
+                  // Gives each series an axis name that matches the Y-axis below.
+                  0: { axis: "diffChangeInOpenInterest" },
+                  1: { axis: "underlyingValue" },
+                },
+                axes: {
+                  // Adds labels to each axis; they don't have to match the axis names.
+                  y: {
+                    diffChangeInOpenInterest: {
+                      label: "diffChangeInOpenInterest",
+                    },
+                    underlyingValue: { label: "underlyingValue" },
+                  },
+                },
+              }}
+              rootProps={{ "data-testid": "4" }}
+            />
+          </div>
         </>
       ) : null}
     </div>
